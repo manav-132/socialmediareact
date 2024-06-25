357799d8fdb2acaeb84f795232c989e7146109a2
Manav joshi <manavjoshi1107@gmail.com>
2024-06-26 00:07:04 +0530

    Your descriptive commit message3

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import replace from '@rollup/plugin-replace';
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-    replace({
-      'process.env': JSON.stringify(process.env),
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      external: ["@material-ui/icons"]
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["@material-ui/icons"]
+  },
+  resolve: {
+    alias: {
+      '@material-ui/icons': '@material-ui/icons/esm'
     }
   }
 })
-
-
-
-
-
-
